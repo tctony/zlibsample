9d0f6182f96709a0a9d095fc559b906839a06b07
Tony Tang <tangchang21@gmail.com>
2018-12-19 23:41:48 +0800

    finished the sample

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <assert.h>
 #include "zlib.h"
 
-#define CHUNK (1024 * 16)
+#define CHUNK (1024 * 100)
 
 int deflateFile(FILE* source, FILE* dest, int level)
 {
@@ -33,13 +33,125 @@ int deflateFile(FILE* source, FILE* dest, int level)
     do {
       strm.avail_out = CHUNK;
       strm.next_out = out;
-      
+
+      ret = deflate(&strm, flush);
+      assert(ret != Z_STREAM_ERROR);
+
+      have = CHUNK - strm.avail_out;
+      if (fwrite(out, 1, have, dest) != have || ferror(dest)) {
+        (void)deflateEnd(&strm);
+        return Z_ERRNO;
+      }
+    } while(strm.avail_out == 0);
+    assert(strm.avail_in == 0);
+  } while(flush != Z_FINISH);
+  assert(ret == Z_STREAM_END);
+
+  (void)deflateEnd(&strm);
+  return Z_OK;
+}
+
+int inflateFile(FILE* source, FILE* dest)
+{
+  int ret;
+  unsigned have;
+  z_stream strm;
+  unsigned char in[CHUNK];
+  unsigned char out[CHUNK];
+
+  strm.zalloc = Z_NULL;
+  strm.zfree = Z_NULL;
+  strm.opaque = Z_NULL;
+  strm.avail_in = 0;
+  strm.next_in = Z_NULL;
+  ret = inflateInit(&strm);
+  if (ret != Z_OK) {
+    return ret;
+  }
+
+  do {
+    strm.avail_in = fread(in, 1, CHUNK, source);
+    if (ferror(source)) {
+      (void)inflateEnd(&strm);
+      return Z_ERRNO;
     }
+    if (strm.avail_in == 0)
+      break;
+    strm.next_in = in;
+
+    do {
+      strm.avail_out = CHUNK;
+      strm.next_out = out;
+      ret = inflate(&strm, Z_NO_FLUSH);
+      assert(ret != Z_STREAM_ERROR);
+      switch (ret) {
+        case Z_NEED_DICT:
+          ret = Z_DATA_ERROR;
+        case Z_DATA_ERROR:
+        case Z_MEM_ERROR:
+          (void)inflateEnd(&strm);
+          return ret;
+      }
+
+      have = CHUNK - strm.avail_out;
+      if (fwrite(out, 1, have, dest) != have || ferror(dest)) {
+        (void)inflateEnd(&strm);
+        return Z_ERRNO;
+      }
+    } while(strm.avail_out == 0);
+  } while(ret != Z_STREAM_END);
+
+  (void)inflateEnd(&strm);
+
+  return ret == Z_STREAM_END ? Z_OK : Z_DATA_ERROR;
+}
+
+void zerr(int ret)
+{
+  fputs("zpipe: ", stderr);
+  switch (ret) {
+    case Z_ERRNO:
+      if (ferror(stdin))
+        fputs("error reading stdin\n", stderr);
+      if (ferror(stdout))
+        fputs("error writing stdout\n", stderr);
+      break;
+    case Z_STREAM_ERROR:
+      fputs("invalid compression level\n", stderr);
+      break;
+    case Z_DATA_ERROR:
+      fputs("invalid or incomplete deflate data\n", stderr);
+      break;
+    case Z_MEM_ERROR:
+      fputs("out of memory\n", stderr);
+      break;
+    case Z_VERSION_ERROR:
+      fputs("zlib version mismatch!\n", stderr);
   }
 }
 
 int main(int argc, char *argv[])
 {
-  
+  int ret;
+
+  if (argc == 1) {
+    ret = deflateFile(stdin, stdout, Z_DEFAULT_COMPRESSION);
+    if (ret != Z_OK) {
+      zerr(ret);
+    }
+    return ret;
+  }
+  else if (argc == 2 && strcmp(argv[1], "-d") == 0) {
+    ret = inflateFile(stdin, stdout);
+    if (ret != Z_OK) {
+      zerr(ret);
+    }
+    return ret;
+  }
+  else {
+    fputs("zpipe usage: zpipe [-d] < source > dest\n", stderr);
+    return 1;
+  }
+
   return 0;
 }
